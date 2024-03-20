e8ab2b0694204749c638717142d30d73475455b8
thibaut.mosteau <thibaut.mosteau@lilo.org>
2024-03-20 11:17:39 +0100

    create inscription and delete info in table emailverification

---
+++ pages/api/emailverification/delete-emailverification.ts
@@ -0,0 +1,27 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  console.log('Requête reçue', req.method, req.query);
+
+  if (req.method === "DELETE") {
+    const { email } = req.query;
+
+    try {
+      const deletedEmailVerification = await prisma.emailVerification.delete({
+        where: { email: email as string },
+      });
+
+      return res.json(deletedEmailVerification);
+    } catch (err) {
+      return res.status(500).json({ error: (err as Error).message });
+    }
+  } else {
+    res.status(405).json({ error: "Méthode non autorisée" });
+  }
+}
