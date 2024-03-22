804c1bdb039bd369a1d411b4d35f455bc054fac4
thibaut.mosteau <thibaut.mosteau@lilo.org>
2024-03-22 16:24:39 +0100

    fetch cards by user

---
+++ pages/api/auth/authMiddleware.ts
@@ -0,0 +1,30 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import jwt from 'jsonwebtoken';
+
+interface NextApiRequestWithAuth extends NextApiRequest {
+  user: string | object; // Change 'auth' to 'user'
+}
+
+export default function verifyToken(req: NextApiRequestWithAuth, res: NextApiResponse, next: () => void) {
+  try {
+    const authorizationHeader = req.headers.authorization;
+
+    if (!authorizationHeader) {
+      throw new Error("Authorization header is missing");
+    }
+
+    const [type, token] = authorizationHeader.split(" ");
+
+    if (type !== "Bearer") {
+      throw new Error("Authorization header has not the 'Bearer' type");
+    }
+
+    req.user = jwt.verify(token, process.env.APP_SECRET); // Change 'auth' to 'user'
+
+    next();
+  } catch (err) {
+    console.error(err);
+
+    res.status(401).end();
+  }
+}
