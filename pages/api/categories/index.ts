11e106c6a2cb930cf5e662b170b8586728eeb6fa
thibaut.mosteau <thibaut.mosteau@lilo.org>
2024-03-28 14:40:39 +0100

    create local storage

--- pages/api/categories/index.ts
+++ pages/api/categories/index.ts
@@ -5,8 +5,9 @@ import verifyToken from "../../api/auth/authMiddleware";
 const prisma = new PrismaClient();
 
 interface CustomNextApiRequest extends NextApiRequest {
-  user: { userId: number; iat: number; exp: number }; // Update to reflect the correct structure
+  user: { userId: number; iat: number; exp: number };
 }
+
 
 export default function handle(
   req: CustomNextApiRequest,
@@ -19,11 +20,10 @@ export default function handle(
       return res.status(405).json({ message: "Method not allowed" });
     }
 
-    const { userId } = req.user; // Destructuring to get userId
+    const { userId } = req.user; 
     console.log("user:", req.user);
 
     if (!userId) {
-      // Checking for userId instead of user directly
       return res.status(401).json({ message: "Not authenticated" });
     }
 
@@ -31,14 +31,12 @@ export default function handle(
       console.log("Fetching categories for user ID:", userId);
       const categories = await prisma.category.findMany({
         where: {
-          userId: userId, // Using userId
+          userId: userId,
         },
         include: {
-          color: true, // Include the related color
+          color: true,
         },
       });
-
-      // Map over the categories to replace colorId with color name
       const categoriesWithColorName = categories.map((category) => ({
         ...category,
         colorName: category.color.name,
