50d3b47b156cd52b4b843583308e8556d090cac4
Thibaut Mosteau <thibaut.mosteau@lilo.org>
2024-05-13 16:02:33 +0200

    modif email  and page check email ok

---
+++ pages/api/users/verify-email.ts
@@ -0,0 +1,73 @@
+import { PrismaClient } from "@prisma/client";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+const prisma = new PrismaClient();
+
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { token, email } = req.query;
+
+  if (req.method === "GET") {
+    if (!token || !email) {
+      return res.status(400).json({ error: "Token or email is not defined" });
+    }
+
+    try {
+      const emailVerification = await prisma.emailVerification.findUnique({
+        where: {
+          token: token as string,
+        },
+      });
+
+      console.log("EmailVerification:", emailVerification);
+
+      if (!emailVerification || emailVerification.email !== email) {
+        return res.status(400).json({ error: "Invalid token or email" });
+      }
+
+      const existingUser = await prisma.user.findUnique({
+        where: { email: emailVerification.email },
+      });
+
+      if (existingUser) {
+        return res.status(400).json({ error: "Email is already in use" });
+      }
+
+      if (!emailVerification.userId) {
+        return res.status(400).json({ error: "User ID is not defined" });
+      }
+
+      const userToUpdate = await prisma.user.findUnique({
+        where: { id: emailVerification.userId },
+      });
+
+      if (!userToUpdate) {
+        return res.status(400).json({ error: "User not found" });
+      }
+
+      const updatedUser = await prisma.user.update({
+        where: { id: emailVerification.userId },
+        data: { email: emailVerification.email },
+      });
+
+      console.log("Updated User:", updatedUser);
+
+      const deletedEmailVerification = await prisma.emailVerification.delete({
+        where: { token: token as string },
+      });
+
+      console.log("Deleted EmailVerification:", deletedEmailVerification);
+
+      return res.status(200).json({ message: "Email updated successfully" });
+    } catch (error) {
+      console.error("Error:", error);
+      return res
+        .status(500)
+        .json({ error: "An error occurred while verifying the email" });
+    }
+  } else {
+    res.status(405).json({ error: "We only support GET" });
+  }
+}
