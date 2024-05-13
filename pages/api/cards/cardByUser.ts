fd0388b5f7034cbbbad2f046be45d1c27a87c4bc
Vincent DESBROSSES <vincedsb@gmail.com>
2024-05-13 11:36:04 +0200

    sf-147-2 - deployment error fix 2

--- pages/api/cards/cardByUser.ts
+++ pages/api/cards/cardByUser.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, User } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
 import verifyToken from "../../api/auth/authMiddleware";
 import moment from "moment";
