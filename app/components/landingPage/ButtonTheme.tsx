ac66a81904785876f19febdd580870962e1201e4
Thibaut Mosteau <thibaut.mosteau@lilo.org>
2024-05-23 12:10:24 +0200

    modif component serveur/client

---
+++ app/components/landingPage/ButtonTheme.tsx
@@ -0,0 +1,38 @@
+"use client";
+import React, { useEffect, useState } from 'react';
+import Image from 'next/image';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMoon as farMoon, faSun } from '@fortawesome/free-regular-svg-icons'
+import { useTheme } from 'next-themes';
+import { Button } from "@nextui-org/react";
+
+
+const ButtonTheme = () => {
+    const { theme, setTheme } = useTheme();
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+
+    const handleThemeChange = () => {
+        if (mounted) {
+            setTheme(theme === 'dark' ? 'light' : 'dark');
+        }
+    }
+
+    if (!mounted) return null;
+
+    return (
+        <div id='headerContainer' className='flex items-center justify-between h-16 bg-white-100'>
+            <div className='flex items-center'>
+                <Button id='toggleTheme' className='mr-4' onClick={handleThemeChange} isIconOnly>
+                    <FontAwesomeIcon icon={theme === 'dark' ? faSun : farMoon} />
+                </Button>
+            </div>
+        </div>
+    );
+}
+
+export default ButtonTheme;
