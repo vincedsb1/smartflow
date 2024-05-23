d9668f017901b382fe30dcd5531db5d1aa554721
Vincent DESBROSSES <vincedsb@gmail.com>
2024-05-23 10:45:17 +0200

    debug landing page hero

--- app/components/landingPage/hero.tsx
+++ app/components/landingPage/hero.tsx
@@ -1,34 +1,50 @@
-import React from 'react';
+import React from "react";
 import { Button } from "@nextui-org/react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faRocket } from '@fortawesome/free-solid-svg-icons';
-import { useRouter } from 'next/navigation';
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRocket } from "@fortawesome/free-solid-svg-icons";
+import { useRouter } from "next/navigation";
 
 const Hero = () => {
-    const router = useRouter();
+  const router = useRouter();
 
-    const handleNavigation = () => {
-        router.push("/login");
-    }
+  const handleNavigation = () => {
+    router.push("/login");
+  };
 
-    return (
-        <div id='containerHero' className="w-full">
-            <video className="w-full opacity-65" autoPlay loop muted>
-                <source src="/clipstudent.mp4" type="video/mp4" />
-            </video>
-            <div id="containerTitleTextButtun" className='flex flex-col justify-center items-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 gap-4'>
-                <h1 className="text-cyan-950 text-5xl font-bold mb-4 text-center">
-                    Passez moins de temps à apprendre mieux !
-                </h1>
-                <p className="text-cyan-950 text-2xl font-bold mb-4 text-center">SmartFlow vous fait apprendre à petites doses. La méthode la plus efficace, tout simplement.</p>
-                <Button className="bg-cyan-950 text-white w-60" onClick={handleNavigation}>
-                    <FontAwesomeIcon icon={faRocket} />
-                    Let&apos;s go
-                </Button>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div
+      id="containerHero"
+      className="w-full relative flex items-center align-middle h-full p-10"
+    >
+      <video
+        className="w-full h-full opacity-30 absolute top-0 left-0 object-cover"
+        autoPlay
+        loop
+        muted
+      >
+        <source src="/clipstudent.mp4" type="video/mp4" />
+      </video>
+      <div
+        id="containerTitleTextButtun"
+        className="w-1/2 flex flex-col justify-start relative gap-4 p-10 h-full align-middle"
+      >
+        <h1 className="text-cyan-950 dark:text-cyan-100 text-5xl font-bold font-title mb-4 max-w-1/2">
+          Passez moins de temps à apprendre mieux !
+        </h1>
+        <p className="text-cyan-950 dark:text-cyan-100 text-2xl font-title mb-4 max-w-1/2">
+          SmartFlow vous fait apprendre à petites doses. La méthode la plus
+          efficace, tout simplement.
+        </p>
+        <Button
+          className="bg-cyan-950 text-white w-60"
+          onClick={handleNavigation}
+        >
+          <FontAwesomeIcon icon={faRocket} />
+          Let&apos;s go
+        </Button>
+      </div>
+    </div>
+  );
+};
 
 export default Hero;
