7ac4e2b14c2e26947c0c58184303161753e06eda
Thibaut Mosteau <thibaut.mosteau@lilo.org>
2024-05-24 10:24:41 +0200

    tailwind responsive and aos landingpage

--- app/page.tsx
+++ app/page.tsx
@@ -10,23 +10,48 @@ import Reviews from "./components/landingPage/Reviews";
 import HowItWorks from "./components/landingPage/HowItWorks";
 import OptimisationMemory from "./components/landingPage/OptimisationMemory";
 import ButtonConnexion from "./components/landingPage/ButtonConnexion";
-
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 const LandingPage = () => {
+
+  useEffect(() => {
+    AOS.init({
+      duration: 300,
+    });
+  }, []);
+
   return (
     <div className="bg-white dark:bg-neutral-900">
       <div className="sticky top-0 z-10">
         <Header />
       </div>
-      <Hero />
-      <PrinciplesFeaturesSection />
-      <MidPageGraphic />
-      <PerksCards />
-      <Reviews />
-      <HowItWorks />
-      <OptimisationMemory />
-      <div className="flex items-center justify-center pb-28 mt-16">
-        <ButtonConnexion  label="S'inscrire Gratuitement" />
+      <div data-aos="fade-up">
+        <div data-aos="zoom-out">
+          <Hero />
+        </div>
+        <div data-aos="fade-up">
+          <PrinciplesFeaturesSection />
+        </div>
+        <div data-aos="fade-up">
+          <MidPageGraphic />
+        </div>
+        <div data-aos="fade-up">
+          <PerksCards />
+        </div>
+        <div data-aos="fade-up">
+          <Reviews />
+        </div>
+        <div data-aos="fade-up">
+          <HowItWorks />
+        </div>
+        <div data-aos="fade-up">
+          <OptimisationMemory />
+        </div>
+        <div className="flex items-center justify-center pb-28 mt-16" data-aos="fade-up">
+          <ButtonConnexion label="S'inscrire Gratuitement" />
+        </div>
       </div>
     </div>
   );
