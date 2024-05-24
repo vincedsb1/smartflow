9f4dcf1adf1680e53dd4ae0361a0e4bb4bac9387
Vincent DESBROSSES <vincedsb@gmail.com>
2024-05-24 11:30:18 +0200

    sf-155-8 - Responsive in progress

--- app/components/landingPage/ButtonConnexion.tsx
+++ app/components/landingPage/ButtonConnexion.tsx
@@ -1,23 +1,31 @@
 "use client";
 import { Button } from "@nextui-org/react";
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
 
 interface ButtonConnexionProps {
-    label: string;
+  label: string;
 }
 
 const ButtonConnexion: React.FC<ButtonConnexionProps> = ({ label }) => {
-    const router = useRouter();
+  const router = useRouter();
 
-    const handleNavigation = () => {
-        router.push("/login");
-    }
+  const handleNavigation = () => {
+    router.push("/login");
+  };
 
-    return (
-        <div>
-            <Button onClick={handleNavigation} id="buttonConnexion" color='primary' className='h-10 mr-4' size="lg">{label}</Button>
-        </div>
-    );
-}
+  return (
+    <div>
+      <Button
+        onClick={handleNavigation}
+        id="buttonConnexion"
+        color="primary"
+        className="h-10 w-24 xs:w-28 2xs:w-32 sm:w-36 mr-4"
+        size="lg"
+      >
+        {label}
+      </Button>
+    </div>
+  );
+};
 
 export default ButtonConnexion;
