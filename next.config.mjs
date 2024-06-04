323e937d1e51ce4bc89d1f498c7e678fe2d25434
Thibaut Mosteau <thibaut.mosteau@lilo.org>
2024-06-04 14:28:18 +0200

    redirection /

--- next.config.mjs
+++ next.config.mjs
@@ -2,87 +2,87 @@
 const nextConfig = {
   async redirects() {
     return [
-      {
-        source: '/login',
-        destination: '/',
-        permanent: true,
-      },
-      {
-        source: '/add',
-        destination: '/',
-        permanent: true,
-      },
-      {
-        source: '/cgu',
-        destination: '/',
-        permanent: true,
-      },
-      {
-        source: '/connexion',
-        destination: '/',
-        permanent: true,
-      },
-      {
-        source: '/legal',
-        destination: '/',
-        permanent: true,
-      },
-      {
-        source: '/mailauth',
-        destination: '/',
-        permanent: true,
-      },
-      {
-        source: '/onboarding',
-        destination: '/',
-        permanent: true,
-      },
-      {
-        source: '/organize',
-        destination: '/',
-        permanent: true,
-      },
-      {
-        source: '/register',
-        destination: '/',
-        permanent: true,
-      },
-      {
-        source: '/register-birthday',
-        destination: '/',
-        permanent: true,
-      },
-      {
-        source: '/register-firstname',
-        destination: '/',
-        permanent: true,
-      },
-      {
-        source: '/register-password',
-        destination: '/',
-        permanent: true,
-      },
-      {
-        source: '/today',
-        destination: '/',
-        permanent: true,
-      },
-      {
-        source: '/user',
-        destination: '/',
-        permanent: true,
-      },
-      {
-        source: '/verify-email',
-        destination: '/',
-        permanent: true,
-      },
-      {
-        source: '/verify-email-prospect',
-        destination: '/',
-        permanent: true,
-      },
-    ]
+    //   {
+    //     source: '/login',
+    //     destination: '/',
+    //     permanent: true,
+    //   },
+    //   {
+    //     source: '/add',
+    //     destination: '/',
+    //     permanent: true,
+    //   },
+    //   {
+    //     source: '/cgu',
+    //     destination: '/',
+    //     permanent: true,
+    //   },
+    //   {
+    //     source: '/connexion',
+    //     destination: '/',
+    //     permanent: true,
+    //   },
+    //   {
+    //     source: '/legal',
+    //     destination: '/',
+    //     permanent: true,
+    //   },
+    //   {
+    //     source: '/mailauth',
+    //     destination: '/',
+    //     permanent: true,
+    //   },
+    //   {
+    //     source: '/onboarding',
+    //     destination: '/',
+    //     permanent: true,
+    //   },
+    //   {
+    //     source: '/organize',
+    //     destination: '/',
+    //     permanent: true,
+    //   },
+    //   {
+    //     source: '/register',
+    //     destination: '/',
+    //     permanent: true,
+    //   },
+    //   {
+    //     source: '/register-birthday',
+    //     destination: '/',
+    //     permanent: true,
+    //   },
+    //   {
+    //     source: '/register-firstname',
+    //     destination: '/',
+    //     permanent: true,
+    //   },
+    //   {
+    //     source: '/register-password',
+    //     destination: '/',
+    //     permanent: true,
+    //   },
+    //   {
+    //     source: '/today',
+    //     destination: '/',
+    //     permanent: true,
+    //   },
+    //   {
+    //     source: '/user',
+    //     destination: '/',
+    //     permanent: true,
+    //   },
+    //   {
+    //     source: '/verify-email',
+    //     destination: '/',
+    //     permanent: true,
+    //   },
+    //   {
+    //     source: '/verify-email-prospect',
+    //     destination: '/',
+    //     permanent: true,
+    //   },
+  ]
   },
 };
 
