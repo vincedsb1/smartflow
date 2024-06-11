b1fc5ed5e64e4fca9890415108ee1849e29ecf72
Vincent DESBROSSES <vincedsb@gmail.com>
2024-06-11 16:33:12 +0200

    responsive mobile OK on Review

---
+++ app/components/review/ReviewQuestionButtons.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { Button } from "@nextui-org/button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRandom, faForward } from "@fortawesome/free-solid-svg-icons";
+
+const ReviewQuestionButtons: React.FC<{
+  handleShuffle: () => void;
+  handleShowAnswer: () => void;
+  handleNextCard: () => void;
+}> = ({ handleShuffle, handleShowAnswer, handleNextCard }) => (
+  <div
+    id="controlButtonContainer"
+    className="flex flex-row justify-between gap-2 w-full "
+  >
+    <Button
+      color="default"
+      size="lg"
+      onClick={handleShuffle}
+      isIconOnly
+      className="font-bold font-text"
+      radius="lg"
+    >
+      <FontAwesomeIcon icon={faRandom} />
+    </Button>
+    <Button
+      type="submit"
+      color="primary"
+      variant="solid"
+      size="lg"
+      className="font-bold font-text dark:text-white w-full"
+      onClick={handleShowAnswer}
+      radius="lg"
+    >
+      Réponse
+    </Button>
+    <Button
+      id="nextContainer"
+      color="default"
+      size="lg"
+      onClick={handleNextCard}
+      isIconOnly
+      className="font-bold font-text"
+      radius="lg"
+    >
+      <FontAwesomeIcon icon={faForward} />
+    </Button>
+  </div>
+);
+
+export default ReviewQuestionButtons;
