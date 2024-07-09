ea0ba3cf18c1d46abf9f0994355a30bd83b046cd
Vincent DESBROSSES <vincedsb@gmail.com>
2024-07-09 10:54:08 +0200

    sf-215 - Responsive Confirmation Screen + debug reload /add

--- app/components/add/ConfirmationScreen.tsx
+++ app/components/add/ConfirmationScreen.tsx
@@ -5,24 +5,55 @@ import CardAppTitle from "../CardAppTitle";
 
 const ConfirmationScreen = () => {
   return (
-    <div id="addMainContainer" className="flex flex-col justify-between w-full">
-      <div
-        id="addTopContainer"
-        className="flex flex-col justify-center w-full "
-      >
+    <div id="confirmationScreenMainContainer" className="h-full">
+      <div id="confirmationScreenDesktop" className="hidden sm:block h-full">
         <div
-          id="addHeaderContainer"
-          className="flex flex-col justify-center items-center w-full"
+          id="addMainContainer"
+          className="flex flex-col justify-center w-full h-1/2"
         >
-          {/* <div id="addTitle" className="flex flex-col w-16/20 ">
+          <div
+            id="addTopContainer"
+            className="flex flex-col justify-center w-full "
+          >
+            <div
+              id="addHeaderContainer"
+              className="flex flex-col justify-center items-center w-full"
+            >
+              <div id="addHint" className="flex flex-col items-center w-64">
+                <CardAppText
+                  text="Fiche enregistrée"
+                  icon={faCheck}
+                  iconColor="confirmation"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div id="confirmationScreenMobile" className="sm:hidden">
+        <div
+          id="addMainContainer"
+          className="flex flex-col justify-between w-full"
+        >
+          <div
+            id="addTopContainer"
+            className="flex flex-col justify-center w-full "
+          >
+            <div
+              id="addHeaderContainer"
+              className="flex flex-col justify-center items-center w-full"
+            >
+              {/* <div id="addTitle" className="flex flex-col w-16/20 ">
             <CardAppTitle title="Nouvelle fiche" size="big" />
           </div> */}
-          <div id="addHint" className="flex flex-col items-center w-16/20">
-            <CardAppText
-              text="Fiche enregistrée"
-              icon={faCheck}
-              iconColor="confirmation"
-            />
+              <div id="addHint" className="flex flex-col items-center w-16/20">
+                <CardAppText
+                  text="Fiche enregistrée"
+                  icon={faCheck}
+                  iconColor="confirmation"
+                />
+              </div>
+            </div>
           </div>
         </div>
       </div>
