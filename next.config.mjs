49e24eeb280270ada231e911b0851ee1c6a3d477
Vincent DESBROSSES <vincedsb@gmail.com>
2024-07-10 14:25:13 +0200

    sf-223 - Opening every page in prod

--- next.config.mjs
+++ next.config.mjs
@@ -94,86 +94,86 @@ const nextConfig = {
     if (process.env.NEXT_PUBLIC_ENV === "prod") {
       // Redirections spécifiques pour l'environnement de production
       return [
-        {
-          source: "/login",
-          destination: "/",
-          permanent: true,
-        },
-        {
-          source: "/add",
-          destination: "/",
-          permanent: true,
-        },
-        {
-          source: "/cgu",
-          destination: "/",
-          permanent: true,
-        },
-        {
-          source: "/connexion",
-          destination: "/",
-          permanent: true,
-        },
-        {
-          source: "/legal",
-          destination: "/",
-          permanent: true,
-        },
-        {
-          source: "/mailauth",
-          destination: "/",
-          permanent: true,
-        },
-        {
-          source: "/onboarding",
-          destination: "/",
-          permanent: true,
-        },
-        {
-          source: "/organize",
-          destination: "/",
-          permanent: true,
-        },
-        {
-          source: "/register",
-          destination: "/",
-          permanent: true,
-        },
-        {
-          source: "/register-birthday",
-          destination: "/",
-          permanent: true,
-        },
-        {
-          source: "/register-firstname",
-          destination: "/",
-          permanent: true,
-        },
-        {
-          source: "/register-password",
-          destination: "/",
-          permanent: true,
-        },
-        {
-          source: "/today",
-          destination: "/",
-          permanent: true,
-        },
-        {
-          source: "/user",
-          destination: "/",
-          permanent: true,
-        },
-        {
-          source: "/login",
-          destination: "/",
-          permanent: true,
-        },
-        {
-          source: "/verify-email",
-          destination: "/",
-          permanent: true,
-        },
+        // {
+        //   source: "/login",
+        //   destination: "/",
+        //   permanent: true,
+        // },
+        // {
+        //   source: "/add",
+        //   destination: "/",
+        //   permanent: true,
+        // },
+        // {
+        //   source: "/cgu",
+        //   destination: "/",
+        //   permanent: true,
+        // },
+        // {
+        //   source: "/connexion",
+        //   destination: "/",
+        //   permanent: true,
+        // },
+        // {
+        //   source: "/legal",
+        //   destination: "/",
+        //   permanent: true,
+        // },
+        // {
+        //   source: "/mailauth",
+        //   destination: "/",
+        //   permanent: true,
+        // },
+        // {
+        //   source: "/onboarding",
+        //   destination: "/",
+        //   permanent: true,
+        // },
+        // {
+        //   source: "/organize",
+        //   destination: "/",
+        //   permanent: true,
+        // },
+        // {
+        //   source: "/register",
+        //   destination: "/",
+        //   permanent: true,
+        // },
+        // {
+        //   source: "/register-birthday",
+        //   destination: "/",
+        //   permanent: true,
+        // },
+        // {
+        //   source: "/register-firstname",
+        //   destination: "/",
+        //   permanent: true,
+        // },
+        // {
+        //   source: "/register-password",
+        //   destination: "/",
+        //   permanent: true,
+        // },
+        // {
+        //   source: "/today",
+        //   destination: "/",
+        //   permanent: true,
+        // },
+        // {
+        //   source: "/user",
+        //   destination: "/",
+        //   permanent: true,
+        // },
+        // {
+        //   source: "/login",
+        //   destination: "/",
+        //   permanent: true,
+        // },
+        // {
+        //   source: "/verify-email",
+        //   destination: "/",
+        //   permanent: true,
+        // },
         // {
         //   source: "/verify-email-prospect",
         //   destination: "/",
